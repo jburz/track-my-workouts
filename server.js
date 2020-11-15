a3f7bc60e82a1faf9270e88ee1ecb2a3d264e757
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-11-15 14:51:31 -0700

    require morgan use it to log server requests

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
+const logger = require("morgan");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
