b2b006184eeaac70223b892ced73b7414e29da54
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-11-16 19:17:54 -0700

    mongoose get query returning all records

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,24 @@
+let db = require("../models/workoutModel.js");
+
 module.exports = function (app) {
     app.get("/api/workouts", (req, res) => {
         console.log('hit getLastWorkout()');
         console.log(req.body);
+        db.Workout.find({})
+            .then(workouts => {
+                console.log('hit db.Workout.find');
+                console.log(workouts);
+                res.json(workouts);
+            })
+            .catch(err => {
+                console.log(err);
+                res.json(err);
+            });
+
     });
 
     app.put("/api/workouts/:id", (req, res) => {
+        console.log(req.params);
         console.log('hit /api/workouts put request' + req.params.id);
         console.log(req.body);
     });
@@ -23,6 +37,4 @@ module.exports = function (app) {
         console.log('hit /api/workouts/range post request');
         console.log(req.body);
     });
-
-
 };
